3fab975523dc13e9dfcaab5312467b7c013e2fd4
bbeizer <89622436+bbeizer@users.noreply.github.com>
2022-10-31 16:45:25 -0400

    Add files via upload

---
+++ PA03_CaesarCipher/src/main/java/edu/brandeis/cs12b/pa3/MultiCaesarCipher.java
@@ -0,0 +1,23 @@
+package edu.brandeis.cs12b.pa3;
+
+class MultiCaesarCipher{
+	/**
+	 * This is method to encode a string with multiple shift digits
+	 * @param s the string to be encoded
+	 * @param shifts the set of multiple shift digits
+	 * @return String the string encoded
+	 */
+	public static String multiEncode(String s, int[] shifts){
+		return null;
+	}
+	
+	/**
+	 * This is method to decode a string with multiple shift digits
+	 * @param s the string to be decoded
+	 * @param shifts the set of multiple shift digits
+	 * @return String the string decoded
+	 */
+	public static String multiDecode(String s, int[] shifts){
+		return null;	
+	}
+}
